d3367ef7c45e310e354ae5d89a9bf1f51a7a10fe
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-11-25 20:23:01 +0300

    Exercise_1 completed

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int yourNumber = GetNumber("Enter your number ");
+FindSecondDigit(yourNumber);
+
+
+void FindSecondDigit(int a)
+{
+    int secondDigit = (a % 100) / 10;
+    Console.WriteLine($"{a} -> {secondDigit}");
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
